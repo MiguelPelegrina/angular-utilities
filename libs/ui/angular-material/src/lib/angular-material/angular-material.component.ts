9bb66a9e0d09706cf0569cfd81b516131d4127b0
MiguelPelegrina <95084406+MiguelPelegrina@users.noreply.github.com>
2025-04-01 13:27:25 +0200

    added angular material library

---
+++ libs/ui/angular-material/src/lib/angular-material/angular-material.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-angular-material',
+  imports: [CommonModule],
+  templateUrl: './angular-material.component.html',
+  styleUrl: './angular-material.component.scss',
+})
+export class AngularMaterialComponent {}
