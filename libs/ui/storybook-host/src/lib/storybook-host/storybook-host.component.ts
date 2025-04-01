4f4b21601d1a08567c90bbbcdc7c6b1e46b3169c
MiguelPelegrina <95084406+MiguelPelegrina@users.noreply.github.com>
2025-04-01 13:31:06 +0200

    added storybook host library

---
+++ libs/ui/storybook-host/src/lib/storybook-host/storybook-host.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-storybook-host',
+  imports: [CommonModule],
+  templateUrl: './storybook-host.component.html',
+  styleUrl: './storybook-host.component.scss',
+})
+export class StorybookHostComponent {}
