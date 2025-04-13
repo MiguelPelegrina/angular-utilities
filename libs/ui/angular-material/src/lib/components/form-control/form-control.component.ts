bafd119a4ad2891b240e334c2e706cafe5fef474
MiguelPelegrina <95084406+MiguelPelegrina@users.noreply.github.com>
2025-04-13 20:29:52 +0200

    abstract form controls to form-control

---
+++ libs/ui/angular-material/src/lib/components/form-control/form-control.component.ts
@@ -0,0 +1,35 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { FormElement } from '../../models/form-element.model';
+import { FormGroup } from '@angular/forms';
+import { SharedModule } from '../../shared/shared.module';
+import { MaterialModule } from '../../shared/material.module';
+import { onTimeInput } from '../../utils/time-utils';
+import { ValidationError } from '../../pipes/validation-error.pipe';
+
+@Component({
+  selector: 'lib-form-control',
+  imports: [MaterialModule, SharedModule, ValidationError],
+  templateUrl: './form-control.component.html',
+  styleUrl: './form-control.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FormControlComponent {
+  element = input.required<FormElement>();
+  form = input.required<FormGroup>();
+
+  get isValid() {
+    return this.form().controls[this.element().key].valid;
+  }
+
+  get control() {
+    return this.form().get(this.element().key);
+  }
+
+  /**
+   * Handles time input and automatically formats time input to HH:mm.
+   * @param event
+   */
+  protected onTimeInput(event: Event, controlName: string): void {
+    this.form().get(controlName)?.setValue(onTimeInput(event));
+  }
+}
--- libs/ui/angular-material/src/lib/shared/shared.module.ts
+++ libs/ui/angular-material/src/lib/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ValidationError } from '../pipes/validation-error.pipe';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, ValidationError],
   declarations: [],
   providers: [],
-  exports: [CommonModule, ReactiveFormsModule],
+  exports: [CommonModule, ReactiveFormsModule, ValidationError],
 })
 export class SharedModule {}
